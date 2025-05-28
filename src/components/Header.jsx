2055dea02319ef7ef6cab65efa87e8a6465a943f
Fady Adel <fady.adel2001@gmail.com>
2025-05-28 17:26:07 +0300

    import logos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,15 @@ import {
   LogoImage,
   LogoText,
 } from "../theme/theme";
+import truthifyLogo from "../assets/Icon removed bg.svg";
+import truthifyText from "../assets/Truthify removed bg.svg";
 
 const Header = () => {
   return (
     <HeaderPaper elevation={3}>
       <LogoContainer>
-        <LogoImage src="./src/assets/Icon removed bg.svg" alt="Truthify Logo" />
-        <LogoText
-          src="./src/assets/Truthify removed bg.svg"
-          alt="Truthify Text"
-        />
+        <LogoImage src={truthifyLogo} alt="Truthify Logo" />
+        <LogoText src={truthifyText} alt="Truthify Text" />
       </LogoContainer>
       <Typography variant="h6" sx={{ color: "#7f8c8d", fontSize: "1.1rem" }}>
         AI-Powered Fact Checking

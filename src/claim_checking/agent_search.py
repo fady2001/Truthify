faec0bc229bda89d25126ba318e050870cbe9ef1
arwa-5aled <arwakhaled404@gmail.com>
2025-07-24 04:20:38 +0300

    examples

--- src/claim_checking/agent_search.py
+++ src/claim_checking/agent_search.py
@@ -14,7 +14,7 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-2.5-flash",
     google_api_key=GOOGLE_API_KEY,
     temperature=0.2 
 )
